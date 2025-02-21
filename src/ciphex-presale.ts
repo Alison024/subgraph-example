d45adf80693155542e92f769fbe7b2dd3e548908
Andrii Ublinskykh <andreo.ublin24@gmail.com>
2025-02-21 13:55:58 +0200

    tried to transfer logic from external files to one, not helped, still same issue with invalid lengh of array buffer

--- src/ciphex-presale.ts
+++ src/ciphex-presale.ts
@@ -25,14 +25,56 @@ import {
   TotalContributions,
   UserContribution,
 } from "../generated/schema";
-import {
-  calculateReferralRewards,
-  tokenDecimals,
-  usdDecimals,
-  AffiliateResult,
-} from "./affiliateRewards";
-
+export class Percentage {
+  percentage: BigDecimal;
+  rate: BigDecimal;
+  constructor(percentage: BigDecimal, rate: BigDecimal) {
+    this.percentage = percentage;
+    this.rate = rate;
+  }
+}
+export class AffiliateResult {
+  usdt: BigDecimal;
+  cpx: BigDecimal;
+  constructor(usdt: BigDecimal, cpx: BigDecimal) {
+    this.usdt = usdt;
+    this.cpx = cpx;
+  }
+}
+export class Rank {
+  min: BigDecimal;
+  max: BigDecimal;
+  rate: BigDecimal;
+  constructor(min: string, max: string, rate: string) {
+    this.min = BigDecimal.fromString(min);
+    this.max = BigDecimal.fromString(max);
+    this.rate = BigDecimal.fromString(rate);
+  }
+}
 export function handleBought(event: BoughtEvent): void {
+  const usdDecimals = new BigInt(1000000);
+  const tokenDecimals = new BigInt(1 * 10 ** 18);
+  const ranks: Rank[] = new Array<Rank>();
+  // rank 1, zero means Infinity
+  ranks.push(new Rank("650000", "0", "0.3"));
+  // rank 2
+  ranks.push(new Rank("490000", "650000", "0.26"));
+  // rank 3
+  ranks.push(new Rank("350000", "490000", "0.24"));
+  // rank 4
+  ranks.push(new Rank("250000", "350000", "0.22"));
+  // rank 5
+  ranks.push(new Rank("175000", "250000", "0.2"));
+  // rank 6
+  ranks.push(new Rank("115000", "175000", "0.18"));
+  // rank 7
+  ranks.push(new Rank("75000", "115000", "0.16"));
+  // rank 8
+  ranks.push(new Rank("45000", "75000", "0.14"));
+  // rank 9
+  ranks.push(new Rank("24000", "45000", "0.12"));
+  // rank 10
+  ranks.push(new Rank("0", "24000", "0.1"));
   // handling Bought
   let entity = new Bought(event.transaction.hash.toHex());
   entity.recipient = event.params.recipient;
@@ -91,12 +133,57 @@ export function handleBought(event: BoughtEvent): void {
       entity.ciphexAmount
     );
 
-    /* log.info("Before calculateReferralRewards", []);
+    log.info("Before calculateReferralRewards", []);
     // handling Referral rewards
-    let rewRes: AffiliateResult = calculateReferralRewards(
-      referral.totalUsdContribution,
-      referral.totalCiphexContribution
+    const zeroBd = BigDecimal.fromString("0");
+    const twoBd = BigDecimal.fromString("2");
+    const usdcAmountBi: BigDecimal = new BigDecimal(
+      referral.totalUsdContribution.div(usdDecimals)
     );
+    const cpxAmountBi: BigDecimal = new BigDecimal(
+      referral.totalCiphexContribution.div(tokenDecimals)
+    );
+    let total: BigDecimal = new BigDecimal(
+      referral.totalUsdContribution.div(usdDecimals)
+    );
+    let usdRewards: BigDecimal = BigDecimal.fromString("0");
+    let percentages: Percentage[] = [];
+    let ranksLength = ranks.length;
+    for (let i = ranksLength - 1; i >= 0; i--) {
+      if (total == zeroBd) break;
+      const rank: Rank | null = ranks[i];
+      if (!rank) break;
+      // Calculate the maximum amount that can be attributed to this rank
+      let bracketMax: BigDecimal;
+      // same as const bracketMax = Math.min(rank.max - rank.min + 1, total);
+      if (rank.max == zeroBd) {
+        bracketMax = total;
+      } else {
+        const delta = rank.max.minus(rank.min);
+        if (delta < total) {
+          bracketMax = delta;
+        } else {
+          bracketMax = total;
+        }
+      }
+      // Compute commission based on the rank's rate
+      const commission: BigDecimal = bracketMax.times(rank.rate.div(twoBd)); // = bracketMax * (rate / 2)
+      usdRewards = usdRewards.plus(commission);
+
+      // Track the percentage of the total contribution for this rank
+      percentages.push(
+        new Percentage(bracketMax.div(usdcAmountBi), rank.rate.div(twoBd))
+      );
+      // Deduct the processed amount from the remaining total
+      total = total.minus(bracketMax);
+    }
+    let cpxRewards: BigDecimal = BigDecimal.fromString("0");
+    for (let i = 0; i < percentages.length; i++) {
+      cpxRewards = cpxRewards.plus(
+        cpxAmountBi.times(percentages[i].percentage.times(percentages[i].rate))
+      );
+    }
+    let rewRes: AffiliateResult = new AffiliateResult(usdRewards, cpxRewards);
     log.info("After calculateReferralRewards", []);
     let usdRewDelta: BigInt = BigInt.fromString(
       rewRes.usdt.truncate(0).toString()
@@ -114,7 +201,7 @@ export function handleBought(event: BoughtEvent): void {
     );
     referral.totalCiphexRewards = BigInt.fromString(
       rewRes.cpx.truncate(0).toString()
-    ); */
+    );
     log.info("After updating total rewards", []);
     let totalAffiliateRewards = new TotalAffiliateRewards(zeroAddress);
     if (!totalAffiliateRewards) {
@@ -122,10 +209,10 @@ export function handleBought(event: BoughtEvent): void {
       totalAffiliateRewards.totalUsdRewards = BigInt.fromI32(0);
       totalAffiliateRewards.totalCiphexRewards = BigInt.fromI32(0);
     }
-    // totalAffiliateRewards.totalUsdRewards =
-    //   totalAffiliateRewards.totalUsdRewards.plus(usdRewDelta);
-    // totalAffiliateRewards.totalCiphexRewards =
-    //   totalAffiliateRewards.totalCiphexRewards.plus(cpxRewDelta);
+    totalAffiliateRewards.totalUsdRewards =
+      totalAffiliateRewards.totalUsdRewards.plus(usdRewDelta);
+    totalAffiliateRewards.totalCiphexRewards =
+      totalAffiliateRewards.totalCiphexRewards.plus(cpxRewDelta);
     referral.save();
     totalAffiliateRewards.save();
   }
